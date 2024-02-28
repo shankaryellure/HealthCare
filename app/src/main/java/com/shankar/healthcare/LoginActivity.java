d2f1c7fff31ebcce017fd21e68a00e7da4e5ac63
shankaryellure <shankar.yellure511@gmail.com>
2024-02-28 16:43:44 -0500

    initial commit

---
+++ app/src/main/java/com/shankar/healthcare/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.shankar.healthcare;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+    }
+}
